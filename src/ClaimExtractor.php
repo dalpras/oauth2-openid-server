b4bd7a3ebd910728957b2aa91f80dd8af44543ef
root <Stefano Dal Pr>
2021-02-15 08:27:44 +0000

    extending ArrayObject

--- src/ClaimExtractor.php
+++ src/ClaimExtractor.php
@@ -12,154 +12,34 @@ use League\OAuth2\Server\Entities\ScopeEntityInterface;
  * It's possible to add a custom ClaimSet and "extract" using defined scopes.
  * It's not possible to add a custom ClaimSet already present as a standard ClaimSet.
  */
-class ClaimExtractor
-{
-    /**
-     * @var array
-     */
-    protected $claimSets = [];
-
-    /**
-     * These claims cannot be used for user purposes. 
-     * They are valorized by default.
-     * 
-     * @var array
-     */
-    protected $protectedClaims = ['openid', 'profile', 'email', 'address', 'phone'];
-
-    /**
-     * ClaimExtractor constructor.
-     *
-     * @param ClaimSetEntity[] $claimSets
-     */
-    public function __construct($claimSets = [])
-    {
-        // Add Default OpenID Claims
-        // @see http://openid.net/specs/openid-connect-core-1_0.html#ScopeClaims
-        $this->addClaimSet(
-            new ClaimSetEntity('openid', [
-                'sub'
-            ])
-        );
-
-        $this->addClaimSet(
-            new ClaimSetEntity('profile', [
-                'name',
-                'family_name',
-                'given_name',
-                'middle_name',
-                'nickname',
-                'preferred_username',
-                'profile',
-                'picture',
-                'website',
-                'gender',
-                'birthdate',
-                'zoneinfo',
-                'locale',
-                'updated_at'
-            ])
-        );
-        $this->addClaimSet(
-            new ClaimSetEntity('email', [
-                'email',
-                'email_verified'
-            ])
-        );
-        $this->addClaimSet(new ClaimSetEntity('address', ['address']));
-
-        $this->addClaimSet(
-            new ClaimSetEntity('phone', [
-                'phone_number',
-                'phone_number_verified'
-            ])
-        );
-
-        // add custom ClaimSets
-        foreach ($claimSets as $claimSet) {
-            $this->addClaimSet($claimSet);
-        }
-    }
-
-    /**
-     * @param ClaimSetEntityInterface $claimSet
-     * @return $this
-     * @throws \InvalidArgumentException
-     */
-    public function addClaimSet(ClaimSetEntityInterface $claimSet)
-    {
-        $scope = $claimSet->getScope();
-
-        if (in_array($scope, $this->protectedClaims) && !empty($this->claimSets[$scope])) {
-            throw new \InvalidArgumentException(
-                sprintf("%s is a protected scope and is pre-defined by the OpenID specification.", $scope)
-            );
-        }
-
-        $this->claimSets[$scope] = $claimSet;
-
-        return $this;
-    }
-
-    /**
-     * @param string $scope
-     * @return ClaimSetEntity|null
-     */
-    public function getClaimSet($scope)
-    {
-        if (!$this->hasClaimSet($scope)) {
-            return null;
-        }
-
-        return $this->claimSets[$scope];
-    }
-
-    /**
-     * @param string $scope
-     * @return bool
-     */
-    public function hasClaimSet($scope)
-    {
-        return array_key_exists($scope, $this->claimSets);
-    }
-
+class ClaimExtractor extends \ArrayObject {
+    
     /**
      * For given scopes and aggregated claims get all claims that have been configured on the extractor.
-     *
-     * @param array $scopes
-     * @param array $claims
-     * @return array
+     * 
+     * @param array $userScopes
+     * @param array $userClaims
+     * @return array|mixed
      */
-    public function extract(array $scopes, array $claims)
-    {
-        $claimData  = [];
-        $keys       = array_keys($claims);
-
-        foreach ($scopes as $scope) {
-            $scopeName = ($scope instanceof ScopeEntityInterface) ? $scope->getIdentifier() : $scope;
-
-            $claimSet = $this->getClaimSet($scopeName);
-            if (null === $claimSet) {
-                continue;
+    public function extract(array $userScopes, array $userClaims) {
+        $claims = array_reduce($this->getArrayCopy(), function ($carry, $claimSet) use (&$userScopes, $userClaims) {
+            /* @var $scope ScopeEntityInterface */
+            foreach ($userScopes as $key => $userScope) {
+                $scopeName = ($userScope instanceof ScopeEntityInterface) ? $userScope->getIdentifier() : $userScope;
+                /* @var $claimSet ClaimSetEntityInterface */
+                if ( $scopeName === $claimSet->getScope() ) {
+                    // if the userScope was matched
+                    unset($userScopes[$key]);
+                    $data = array_intersect_key($userClaims, array_fill_keys($claimSet->getClaims(), 1));
+                    $carry = array_replace($carry, $data);
+                    return $carry;
+                }
             }
-
-            // get the claims that are only allowed by the scope
-            $common = array_intersect($claimSet->getClaims(), $keys);
-
-            if (empty($common)) {
-                continue;
-            }
-
-            $data = array_filter($claims,
-                function($key) use ($common) {
-                    return in_array($key, $common);
-                },
-                ARRAY_FILTER_USE_KEY
-            );
-
-            $claimData = array_merge($claimData, $data);
-        }
-
-        return $claimData;
+            return $carry;
+        }, []);
+        
+        // if all userScopes were statisfied, return the claims collected, instead return nothing
+        return empty($userScopes) ? $claims : [];
     }
+    
 }
